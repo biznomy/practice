9c2c5f43f0cf3c1190843c90563dc24577e87f95
raj-kumarraj <kumar.kumarraj1989@gmail.com>
2018-12-31 15:58:24 +0530

    checkbox paging

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,24 +1,20 @@
-var User = require('../models/user.model.js');
+var User = require("../models/user.model.js");
 var mongoose = require("mongoose");
-var express = require('express');
+var express = require("express");
 var app = express();
 
 var multer = require("multer");
-const path   = require('path');
+const path = require("path");
 
+var ObjectId;
+exports.mongoose = mongoose.ObjectId;
 
-
-
-
-var ObjectId ;
-exports.mongoose = mongoose.ObjectId;   
-
-app.use(express.static('./public/uploads/'));
+app.use(express.static("./public/uploads/"));
 
 exports.save = (req, res) => {
-	console.log('Post a User: ' + JSON.stringify(req.body));
+  console.log("Post a User: " + JSON.stringify(req.body));
 
- /*const user = new User({
+  /*const user = new User({
         firstname : req.body.firstname,
         lastname  : req.body.lastname,
         email     : req.body.email,
@@ -38,90 +34,91 @@ exports.save = (req, res) => {
         });
     });*/
 };
-var listSort = ['time','request'];
+var listSort = ["time", "request"];
 var defSort = listSort[0];
-var listOrder = ['-1','1'];
+var listOrder = ["-1", "1"];
 var defOrder = listOrder[0];
 
-    exports.findAll = (req, res) =>  {
-    console.log("Fetch all Users");
+exports.findAll = (req, res) => {
+  console.log("Fetch all Users");
 
+  var rQuery = req.query || {};
+  var limit = rQuery.limit || 5;
+  var page = rQuery.page || 1;
 
+  var sort = listSort.indexOf(rQuery.sort) === -1 ? defSort : rQuery.sort;
+  var order = listOrder.indexOf(rQuery.order) === -1 ? defOrder : rQuery.order;
+  var querySort = {};
+  querySort.sort = parseInt(order, 10);
 
-var rQuery = req.query || {};
-   var limit = rQuery.limit || 5;
-   var page = rQuery.page || 1;
+  var timeBegin = isNaN(Number(rQuery.timeBegin))
+    ? undefined
+    : Number(rQuery.timeBegin);
+  var timeEnd = isNaN(Number(rQuery.timeEnd))
+    ? undefined
+    : Number(rQuery.timeEnd);
+  var request = rQuery.request;
+  var queryFind = {};
 
-   var sort =  listSort.indexOf(rQuery.sort) === -1 ? defSort :  rQuery.sort;
-   var order = listOrder.indexOf(rQuery.order) === -1 ? defOrder : rQuery.order;
-   var querySort = {};
-   querySort.sort = parseInt(order, 10);
+  if (timeBegin || timeEnd) {
+    queryFind.time = {};
+    if (timeBegin) queryFind.time["$gte"] = timeBegin;
+    if (timeEnd) queryFind.time["$lte"] = timeEnd;
+  }
 
-   var timeBegin = isNaN(Number(rQuery.timeBegin)) ? undefined : Number(rQuery.timeBegin);
-   var timeEnd = isNaN(Number(rQuery.timeEnd))  ? undefined : Number(rQuery.timeEnd);
-   var request = rQuery.request;
-   var queryFind = {};
-
-   if(timeBegin || timeEnd){
-       queryFind.time={};
-       if(timeBegin)
-           queryFind.time['$gte']=timeBegin;
-       if(timeEnd)
-           queryFind.time['$lte']=timeEnd;
-   }
-
-   if(request){
-       queryFind.request =  new RegExp(request, 'i');
-   }
-
-   User.find(queryFind).sort(querySort)
-   .limit(limit).skip(limit*(page-1), function(err, logItems) {
-       if (!err) {
-         return res.json(logItems);
-       } else {
-         return res.send({error: err});
-       }
-   })/*paginate({}, options)*/
-    .then(users => {
-        res.send(users);
-    }).catch(err => {
-        res.status(500).send({
-            message: err.message
-        });
-    });
+  if (request) {
+    queryFind.request = new RegExp(request, "i");
+  }
+  // db.users.find({$text:{$search: "raj",$caseSensitive: false}})
+  User.count(queryFind, function(error, count) {
+    if (!error) {
+      User.find(queryFind, function(err, logItems) {
+        if (!err) {
+          return res.json({ results: logItems, count: count });
+        } else {
+          return res.send({ error: err });
+        }
+      })
+        .sort(querySort)
+        .limit(limit)
+        .skip(limit * (page - 1));
+    } else {
+      return res.status(501).send({ error: error });
+    }
+  });
 };
 
 exports.delete = (req, res) => {
-    const user = new User({
-        firstname  : req.body.firstname,
-        lastname   : req.body.lastname,
-        email      : req.body.email,
-        gender     : req.body.gender,
-        contact    : req.body.contact,
-        subject    : req.body.subject,
-        imageURL   : req.body.imageURL
-    });
+  const user = new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    gender: req.body.gender,
+    contact: req.body.contact,
+    subject: req.body.subject,
+    imageURL: req.body.imageURL
+  });
 
-    User.findByIdAndRemove(req.params.id)
+  User.findByIdAndRemove(req.params.id)
     .then(user => {
-        if(!user) {
-            return res.status(404).send({
-                message: "user not found with id " + req.params.id
-            });
-        }
-        res.send({message: "user deleted successfully!" + req.params.id});
-    }).catch(err => {
-        if(err.kind === 'ObjectId' || err.firstname === 'NotFound') {
-            return res.status(404).send({
-                message: "user not found with id " + req.params.id
-            });                
-        }
-        return res.status(500).send({
-            message: "Could not delete user with id " + req.params.id
+      if (!user) {
+        return res.status(404).send({
+          message: "user not found with id " + req.params.id
         });
+      }
+      res.send({ message: "user deleted successfully!" + req.params.id });
+    })
+    .catch(err => {
+      if (err.kind === "ObjectId" || err.firstname === "NotFound") {
+        return res.status(404).send({
+          message: "user not found with id " + req.params.id
+        });
+      }
+      return res.status(500).send({
+        message: "Could not delete user with id " + req.params.id
+      });
     });
 };
-
 
 /*exports.update = (req, res) => {
    
--- resources/static/js/postrequest.js
+++ resources/static/js/postrequest.js
@@ -1,388 +1,345 @@
-$( document ).ready(function() {
-	
-    $("#Submit").click(function(event) {
-		
-		event.preventDefault();
-		ajaxPost();
+$(document).ready(function() {
+  $("#Submit").click(function(event) {
+    event.preventDefault();
+    ajaxPost();
     //setInterval("ajaxPost()", 1000);
-	});
+  });
 
-    
+  function ajaxPost() {
+    firstname = $("#firstname").val();
+    lastname = $("#lastname").val();
+    email = $("#email").val();
+    gender = $('input[name="gender"]:checked').val();
 
-    function ajaxPost(){
-    		firstname = $("#firstname").val();
-    		lastname  = $("#lastname").val();
-    		email 	  = $("#email").val();
-    		gender 	  = $('input[id="gender"]:checked').map(function(){
-                    return $(this).val();
-                     }).get();
-    		contact	  = $("#contact").val();
-    		subject	  = $('input[id="subject"]:checked').map(function(){
-                    return $(this).val();
-                    }).get();
-    					
-    		imageURL  = $("#image")[0].files[0];
+    contact = $("#contact").val();
+    subject = $('input[name="subject"]:checked')
+      .map(function() {
+        return $(this).val();
+      })
+      .get();
 
-    		var formData = new FormData();
-    		formData.append('firstname', firstname);
-    		formData.append('lastname', lastname);
-    		formData.append('email', email);
-    		formData.append('gender', gender);
-    		formData.append('contact', contact);
-    		formData.append('subject', subject);
-    		formData.append('imageURL', imageURL);
-    			
-    	
-    	// {firstname, lastname, email, gender, contact, subject, imageURL}
-    	
-    	$.ajax({
+    imageURL = $("#image")[0].files[0];
 
+    var formData = new FormData();
+    formData.append("firstname", firstname);
+    formData.append("lastname", lastname);
+    formData.append("email", email);
+    formData.append("gender", gender);
+    formData.append("contact", contact);
+    formData.append("subject", subject);
+    formData.append("imageURL", imageURL);
+
+    // {firstname, lastname, email, gender, contact, subject, imageURL}
+
+    $.ajax({
       async: true,
-			type : "POST",
-			url : window.location + "api/users/save",
-			data : formData,
-			dataType: 'JSON',
-			cache: false,
+      type: "POST",
+      url: window.location + "api/users/save",
+      data: formData,
+      dataType: "JSON",
+      cache: false,
       contentType: false,
       processData: false,
-			success : function(user){
-				$("#postResultDiv").html("<h1 style='font-size: 14px; font-style: initial;''>" + 
-					"Data Saved Successfully! " +
-					" " + user.firstname + " " + user.lastname + "</h1>");
-       location.reload();
-			},
-			error : function(e) {  
-				alert("Error!");
-				console.log("ERROR: ",+e);
-			}
-		});
-    	
-    	resetData();
+      success: function(user) {
+        $("#postResultDiv").html(
+          "<h1 style='font-size: 14px; font-style: initial;''>" +
+            "Data Saved Successfully! " +
+            " " +
+            user.firstname +
+            " " +
+            user.lastname +
+            "</h1>"
+        );
+        location.reload();
+      },
+      error: function(e) {
+        alert("Error!");
+        console.log("ERROR: ", +e);
+      }
+    });
+
+    resetData();
+  }
+
+  $("#fn").hide();
+  $("#ln").hide();
+  $("#emailID").hide();
+  $("#sex").hide();
+  $("#contactno").hide();
+  $("#imageID").hide();
+  $("#sub").hide();
+
+  var fn_err = true;
+  var ln_err = true;
+  var email_err = true;
+  var sex_err = true;
+  var contact_err = true;
+  var image_err = true;
+  var sub_err = true;
+
+  $("#firstname").keyup(function() {
+    userFN();
+  });
+
+  $("#lastname").keyup(function() {
+    userLN();
+  });
+
+  $("#email").keyup(function() {
+    userEmailID();
+  });
+
+  $("#contact").keyup(function() {
+    userContact();
+  });
+
+  $("#Submit").click(function() {
+    userGender();
+  });
+
+  $("#Submit").click(function() {
+    userImage();
+  });
+
+  $("#Submit").click(function() {
+    userSubject();
+  });
+
+  function userFN() {
+    var name = /^[a-zA-Z]+$/;
+    var fn = $("#firstname").val();
+
+    if (fn.length == "") {
+      $("#fn").show();
+      $("#fn").html("Please enter First Name");
+      $("#fn").focus();
+      $("#fn").css("color", "red");
+
+      fn_err = false;
+      return true;
+    } else {
+      $("#fn").hide();
     }
+    if (fn.length == 0 || !fn.match(name)) {
+      $("#fn").show();
+      $("#fn").html("Please enter valid values only");
+      $("#fn").focus();
+      $("#fn").css("color", "red");
 
-    $('#fn').hide();
-    $('#ln').hide();
-    $('#emailID').hide();
-    $('#sex').hide();
-    $('#contactno').hide();
-    $('#imageID').hide();
-    $('#sub').hide();
+      fn_err = false;
+      return false;
+    } else {
+      $("#fn").hide();
+    }
+    if (fn.length < 3 || fn.length > 10) {
+      $("#fn").show();
+      $("#fn").html("Please enter First Name 3 between 10");
+      $("#fn").focus();
+      $("#fn").css("color", "red");
 
-	var fn_err = true;
-	var ln_err = true;
-	var email_err = true;
-	var sex_err = true;
-	var contact_err =true;
-	var image_err = true;
-	var sub_err =true;
+      fn_err = false;
+      return false;
+    } else {
+      $("#fn").hide();
+    }
+  }
+  function userLN() {
+    var name = /^[a-zA-Z]+$/;
+    var fn = $("#lastname").val();
 
+    if (fn.length == "") {
+      $("#ln").show();
+      $("#ln").html("Please enter Last name");
+      $("#ln").focus();
+      $("#ln").css("color", "red");
 
+      ln_err = false;
+      return true;
+    } else {
+      $("#ln").hide();
+    }
+    if (fn.length == 0 || !fn.match(name)) {
+      $("#ln").show();
+      $("#ln").html("Please enter valid values only");
+      $("#ln").focus();
+      $("#ln").css("color", "red");
 
-	$("#firstname").keyup(function(){
-		userFN();
-	});
+      ln_err = false;
+      return false;
+    } else {
+      $("#ln").hide();
+    }
+    if (fn.length < 3 || fn.length > 10) {
+      $("#ln").show();
+      $("#ln").html("Please enter Last Name 3 between 10");
+      $("#ln").focus();
+      $("#ln").css("color", "red");
 
-	$('#lastname').keyup(function(){
-		userLN();
-	});
-
-	$('#email').keyup(function(){
-		userEmailID();
-	});
-
-	$('#contact').keyup(function(){
-		userContact();
-	});
-
-	$('#Submit').click(function(){
-
-		userGender();
-
-	});
-
-	$('#Submit').click(function(){
-
-		userImage();
-
-	});
-
-	$('#Submit').click(function(){
-		userSubject();
-	});
-
-	
-
-
-
-	function userFN()
-	{
-		var name = /^[a-zA-Z]+$/;
-		var fn = $('#firstname').val();
-
-		if(fn.length == '')
-		{
-			$('#fn').show();
-			$('#fn').html("Please enter First Name");
-			$('#fn').focus();
-			$('#fn').css("color", "red");
-
-			fn_err = false;
-			return  true;
-		}
-		else
-		{
-			$("#fn").hide();
-		}
-		if((fn.length == 0)||(!fn.match(name)))
-		{
-			$('#fn').show();
-  			$('#fn').html("Please enter valid values only");
-  			$('#fn').focus();
-  			$('#fn').css("color", "red");
-  			
-  			fn_err = false;
-  			return false;
-  		}else
-  		{
-  			$('#fn').hide();
-  		}
-  		if((fn.length < 3 )||(fn.length > 10))
-  		{
-  			$('#fn').show();
-  			$('#fn').html("Please enter First Name 3 between 10");
-  			$('#fn').focus();
-  			$('#fn').css("color", "red");
-  			
-  			fn_err = false;
-  			return false;
-  		}else{
-  			$('#fn').hide();
-  		}
-	}
-	function userLN()
-	{
-		var name = /^[a-zA-Z]+$/;
-		var fn = $('#lastname').val();
-
-		if(fn.length == '')
-		{
-			$('#ln').show();
-			$('#ln').html("Please enter Last name");
-			$('#ln').focus();
-			$('#ln').css("color", "red");
-
-			ln_err = false;
-			return  true;
-		}
-		else
-		{
-			$("#ln").hide();
-		}
-		if((fn.length == 0)||(!fn.match(name)))
-		{
-			$('#ln').show();
-  			$('#ln').html("Please enter valid values only");
-  			$('#ln').focus();
-  			$('#ln').css("color", "red");
-  			
-  			ln_err = false;
-  			return false;
-  		}else
-  		{
-  			$('#ln').hide();
-  		}
-  		if((fn.length < 3 )||(fn.length > 10))
-  		{
-  			$('#ln').show();
-  			$('#ln').html("Please enter Last Name 3 between 10");
-  			$('#ln').focus();
-  			$('#ln').css("color", "red");
-  			
-  			fn_err = false;
-  			return false;
-  		}else{
-  			$('#ln').hide();
-  		}
-	}
-
-	function userEmailID(){
-
-		var emailID = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  		var emailId = $('#email').val();
-
-  		if ((emailId.length == 0 )||(!emailId.match(emailID)))
-  		{
-  			$('#emailID').show();
-  			$('#emailID').html("Please enter valid Email Id");
-  			$('#emailID').focus();
-  			$('#emailID').css("color", "red");
-  			
-  			email_err = false;
-  			return false;
-  		}else
-  		{
-  			$('#emailID').hide();
-  		}
-	}
-
-	function userGender()
-	{
-		{
-  
-  	var gender = $('input[type="radio"]').prop('checked', false);
-
-  	if((gender.length == 0)||(gender.length > 1 ))	
-  	{
-  		$('#sex').show();
-  			$('#sex').html("Please select Gender");
-  			$('#sex').focus();
-  			$('#sex').css("color", "red");
-  			
-  			sex_err = false;
-  			return false;
-  		}else{
-  			$('#sex').hide();
-  		
-  	}
+      fn_err = false;
+      return false;
+    } else {
+      $("#ln").hide();
+    }
   }
 
+  function userEmailID() {
+    var emailID = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var emailId = $("#email").val();
 
-}
+    if (emailId.length == 0 || !emailId.match(emailID)) {
+      $("#emailID").show();
+      $("#emailID").html("Please enter valid Email Id");
+      $("#emailID").focus();
+      $("#emailID").css("color", "red");
 
-function userContact()
-{
-			var num = /^[0-9]+$/;
-	  		var mobile = $('#contact').val();
-
-  		if ((mobile.length == 0 )||(mobile.length > 10 ))
-  		{
-  			$('#contactno').show();
-  			$('#contactno').html("Contact No. must be 10 digit");
-  			$('#contactno').focus();
-  			$('#contactno').css("color", "red");
-  			
-  			contact_err = false;
-  			return false;
-  		}else{
-  			$('#contactno').hide();
-  		}
-  		if (!mobile.match(num))
-  		{
-  			$('#contactno').show();
-  			$('#contactno').html("Please enter valid Contact No:");
-  			$('#contactno').focus();
-  			$('#contactno').css("color", "red");
-  			
-  			contact_err = false;
-  			return false;
-  		}else{
-  			$('#contactno').hide();
-  		}
-
-}
-
-function userImage()
-{
-	var image = $('#image').val();
-  	var file = $('#image')[0].files[0];
-
-  	if(image.length =='')	
-  	{
-  		$('#imageID').show();
-  			$('#imageID').html("Please select image");
-  			$('#imageID').focus();
-  			$('#imageID').css("color", "red");
-  			
-  			image_err = false;
-  			return false;
-  		}else{
-  			$('#imageID').hide();
-  		
-  	}
-  	if(!image.match(/(?:gif|jpg|png|bmp)$/))	
-  	{
-  		$('#imageID').show();
-  			$('#imageID').html("Please select correct format");
-  			$('#imageID').focus();
-  			$('#imageID').css("color", "red");
-  			
-  			image_err = false;
-  			return false;
-  		}else{
-  			$('#imageID').hide();
-  		
-  	}
-  	if(Math.round(file.size / (1024 * 1024)) > 2)	
-  	{
-  		$('#imageID').show();
-  			$('#imageID').html("Size of the image should be below 1 mb ");
-  			$('#imageID').focus();
-  			$('#imageID').css("color", "red");
-  			
-  			image_err = false;
-  			return false;
-  		}else{
-  			$('#imageID').hide();
-  		
-  	}
+      email_err = false;
+      return false;
+    } else {
+      $("#emailID").hide();
+    }
   }
 
-  function userSubject()
-  {
-  	var sub= $('input[type="checkbox"]').prop('checked', false);
-  
-  	  if((sub.length == 0))	
-  	{
-  		$('#sub').show();
-  			$('#sub').html("Please select at least one Subject");
-  			$('#sub').focus();
-  			$('#sub').css("color", "red");
-  			
-  			sub_err = false;
-  			return false;
-  		}else{
-  			$('#sub').hide();	
-  	}
+  function userGender() {
+    {
+      var gender = $('input[type="radio"]').prop("checked", false);
 
+      if (gender.length == 0 || gender.length > 1) {
+        $("#sex").show();
+        $("#sex").html("Please select Gender");
+        $("#sex").focus();
+        $("#sex").css("color", "red");
+
+        sex_err = false;
+        return false;
+      } else {
+        $("#sex").hide();
+      }
+    }
   }
 
+  function userContact() {
+    var num = /^[0-9]+$/;
+    var mobile = $("#contact").val();
 
-  $('#Submit').click(function(e){
+    if (mobile.length == 0 || mobile.length > 10) {
+      $("#contactno").show();
+      $("#contactno").html("Contact No. must be 10 digit");
+      $("#contactno").focus();
+      $("#contactno").css("color", "red");
 
+      contact_err = false;
+      return false;
+    } else {
+      $("#contactno").hide();
+    }
+    if (!mobile.match(num)) {
+      $("#contactno").show();
+      $("#contactno").html("Please enter valid Contact No:");
+      $("#contactno").focus();
+      $("#contactno").css("color", "red");
+
+      contact_err = false;
+      return false;
+    } else {
+      $("#contactno").hide();
+    }
+  }
+
+  function userImage() {
+    var image = $("#image").val();
+    var file = $("#image")[0].files[0];
+
+    if (image.length == "") {
+      $("#imageID").show();
+      $("#imageID").html("Please select image");
+      $("#imageID").focus();
+      $("#imageID").css("color", "red");
+
+      image_err = false;
+      return false;
+    } else {
+      $("#imageID").hide();
+    }
+    if (!image.match(/(?:gif|jpg|png|bmp)$/)) {
+      $("#imageID").show();
+      $("#imageID").html("Please select correct format");
+      $("#imageID").focus();
+      $("#imageID").css("color", "red");
+
+      image_err = false;
+      return false;
+    } else {
+      $("#imageID").hide();
+    }
+    if (Math.round(file.size / (1024 * 1024)) > 2) {
+      $("#imageID").show();
+      $("#imageID").html("Size of the image should be below 1 mb ");
+      $("#imageID").focus();
+      $("#imageID").css("color", "red");
+
+      image_err = false;
+      return false;
+    } else {
+      $("#imageID").hide();
+    }
+  }
+
+  function userSubject() {
+    var sub = $('input[type="checkbox"]').prop("checked", false);
+
+    if (sub.length == 0) {
+      $("#sub").show();
+      $("#sub").html("Please select at least one Subject");
+      $("#sub").focus();
+      $("#sub").css("color", "red");
+
+      sub_err = false;
+      return false;
+    } else {
+      $("#sub").hide();
+    }
+  }
+
+  $("#Submit").click(function(e) {
     e.preventDefault();
     e.stopPropagation();
 
-  fn_err = true;
-	ln_err = true;
-	email_err = true;
-	sex_err = true;
-	contact_err =true;
-	image_err = true;
-	sub_err =true;
+    fn_err = true;
+    ln_err = true;
+    email_err = true;
+    sex_err = true;
+    contact_err = true;
+    image_err = true;
+    sub_err = true;
 
-		userFN();
-		userLN();
-		userEmailID();
-		userContact();
-		userImage();
+    userFN();
+    userLN();
+    userEmailID();
+    userContact();
+    userImage();
 
-		if((fn_err==true)&&(ln_err==true)&&(email_err==true)&&(sex_err==true)&&(contact_err==true)&&(sub_err==true)&&(image_err==true))
-		{
-			return true;
-		}
-		else {
-			return false;
-		}
-
-	});
-    
-    function resetData(){
-    	$("#firstname").val("");
-    	$("#lastname").val("");
-    	$("#email").val("");
-    	$("#gender").val("");
-    	$("#contact").val("");
-    	$("#subject").val("");
-    	$("#image").val("");
+    if (
+      fn_err == true &&
+      ln_err == true &&
+      email_err == true &&
+      sex_err == true &&
+      contact_err == true &&
+      sub_err == true &&
+      image_err == true
+    ) {
+      return true;
+    } else {
+      return false;
     }
-})
+  });
+
+  function resetData() {
+    $("#firstname").val("");
+    $("#lastname").val("");
+    $("#email").val("");
+    $("#gender").val("");
+    $("#contact").val("");
+    $("#subject").val("");
+    $("#image").val("");
+  }
+});
